1fac348fb8ad9f4b65b9a52984e50e7c47de5cca
Hanif Aulia <hansknappeturnon.ha@gmail.com>
2017-11-12 17:29:22 +0800

    dua

--- platforms/browser/www/js/init.js
+++ platforms/browser/www/js/init.js
@@ -10,11 +10,13 @@
 
         $('.carousel.carousel-slider').carousel({ full_width: true });
         $('ul.tabs').tabs('select_tab', 'tab_id');
+        $('.modal').modal();
+
 
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 $(document).ready(function() {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
+
 });
